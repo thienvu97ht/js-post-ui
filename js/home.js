1bb12471506573f3f077146489a223335352cf62
Duong Vu <thien.vu97ht123@gmail.com>
2023-02-21 17:01:52 +0700

    246. Search Posts with debounce

--- js/home.js
+++ js/home.js
@@ -1,7 +1,8 @@
-import postApi from './api/postAPI'
-import { getUlPaginationElement, setTextContent, truncateText } from './utils'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import debounce from 'lodash.debounce'
+import postApi from './api/postAPI'
+import { getUlPaginationElement, setTextContent, truncateText } from './utils'
 dayjs.extend(relativeTime)
 
 function createPostElement(post) {
@@ -10,8 +11,6 @@ function createPostElement(post) {
   // find and clone template
   const postTemplate = document.getElementById('postTemplate')
   if (!postTemplate) return
-  console.log('🏆 ~ createPostElement ~ post', post)
-
   const liElement = postTemplate.content.firstElementChild.cloneNode(true)
   if (!liElement) return
 
@@ -38,7 +37,7 @@ function createPostElement(post) {
 }
 
 function renderPostList(postList) {
-  if (!Array.isArray(postList) || postList.length === 0) return
+  if (!Array.isArray(postList)) return
 
   const ulElement = document.getElementById('postList')
   if (!ulElement) return
@@ -78,6 +77,9 @@ async function handleFilterChange(filterName, filterValue) {
     const url = new URL(window.location)
     url.searchParams.set(filterName, filterValue)
     history.pushState({}, '', url)
+
+    // reset page if needed
+    if (filterName === 'title_like') url.searchParams.set('_page', 1)
 
     // fetch API
     const { data, pagination } = await postApi.getAll(url.searchParams)
@@ -143,18 +145,36 @@ function initURL() {
   history.pushState({}, '', url)
 }
 
+function initSearch() {
+  const searchInput = document.getElementById('searchInput')
+  if (!searchInput) return
+
+  // set default values from query params
+  // title_like
+  const queryParams = new URLSearchParams(window.location.search)
+  if (queryParams.get('title_like')) {
+    searchInput.value = queryParams.get('title_like')
+  }
+
+  const debounceSearch = debounce(
+    (event) => handleFilterChange('title_like', event.target.value),
+    500
+  )
+
+  searchInput.addEventListener('input', debounceSearch)
+}
+
 ;(async () => {
   try {
     // attack click event for links
     initPagination()
+    initSearch()
 
     // set default pagination (_page, _limit) on URL
     initURL()
 
     // render post list based URL params
     const queryParams = new URLSearchParams(window.location.search)
-    // set default query params if not existed
-
     const { data, pagination } = await postApi.getAll(queryParams)
     renderPostList(data)
     renderPagination(pagination)
