dd466b16748b61040bccaed9ceb6c793a2eae083
Duong Vu <thien.vu97ht123@gmail.com>
2023-02-24 15:07:23 +0700

    259. Inititalize form values

--- js/utils/post-form.js
+++ js/utils/post-form.js
@@ -5,6 +5,7 @@ function setFormValues(form, formValues) {
   setFieldValue(form, '[name="author"]', formValues?.author)
   setFieldValue(form, '[name="description"]', formValues?.description)
 
+  setFieldValue(form, '[name="imageUrl"]', formValues?.imageUrl) // hide field
   setBackgroundImage(document, '#postHeroImage', formValues?.imageUrl)
 }
 
